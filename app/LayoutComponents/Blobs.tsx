c6e1ad92a680d509ea50914a170fc7c3e3bfea40
Aaron Harrison <aaronharrison16@gmail.com>
2023-11-17 16:20:40 -0700

    just the bones

---
+++ app/LayoutComponents/Blobs.tsx
@@ -0,0 +1,13 @@
+const Blobs = () => {
+  return (
+    <div className="blob-outer-container">
+      <div className="blob-inner-container">
+        <div className="blob green" />
+        <div className="blob red" />
+        <div className="blob yellow" />
+      </div>
+    </div>
+  )
+}
+
+export default Blobs
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Blobs, Nav } from './LayoutComponents'
+import { Hero } from './Sections'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Nav />
+      
+      <Blobs />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      
     </html>
   )
 }
